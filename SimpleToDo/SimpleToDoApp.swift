1e3b79cb019f25bef1b8478e13e31fe0df145282
akhilapnuri <akhilapnuri@gmail.com>
2026-05-18 18:44:07 -0700

    Initial Commit

---
+++ SimpleToDo/SimpleToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoApp.swift
+//  SimpleToDo
+//
+//  Created by Akhila Pasupunuri on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
